6e8e67e926560f346823adc085d9fc6f209c2851
Leigh Silverstein <lsilversteinto@gmail.com>
2018-02-08 23:31:16 -0500

    remove state

--- src/noreduxActionBatch/index.test.js
+++ src/noreduxActionBatch/index.test.js
@@ -5,14 +5,14 @@ import noreduxActionBatch from './'
 describe('noreduxActionBatch', () => {
   it('returns actions', () => {
     const reducers = {
-      create: payload => state => payload
+      create: payload => () => payload
     }
     const noreduxReducers = noreduxActionBatch(reducers)
     expect(noreduxReducers.create(1)).toMatchSnapshot()
   })
   it('returns actions with noredux wrappings', () => {
     const reducers = {
-      create: payload => state => payload
+      create: payload => () => payload
     }
     const noreduxReducers = noreduxActionBatch(scopeReducers('users', reducers))
     expect(noreduxReducers.create(1)).toMatchSnapshot()
